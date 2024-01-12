be3d01131908376a4578ffa5c7751350ccff1b88
klazapp <klazapp@klazapp.com>
2024-01-12 11:20:08 +0800

    Add files via upload

---
+++ Editor/Data/ScriptCreatorComponent.cs
@@ -0,0 +1,8 @@
+public static class ScriptCreatorComponent 
+{
+    internal const string MONOBEHAVIOUR_TEMPLATE_PATH = "Packages/Klazapp Script Creator/Data/MonoBehaviourTemplate.cs.txt";
+    internal const string MONOBEHAVIOUR_DEFAULT_NAME =  "NewCustomScript.cs";
+
+    internal const string SCRIPTABLEOBJECT_TEMPLATE_PATH = "Packages/Klazapp Script Creator/Data/ScriptableObjectTemplate.cs.txt";
+    internal const string SCRIPTABLEOBJECT_DEFAULT_NAME =  "NewCustomTemplate.cs";
+}
